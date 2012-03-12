45a2ba4af0e361b6907b33c0e708598b084afbd3
Brian Towle <thepoptartprince@gmail.com>
2012-03-11 19:43:32 -0500

    .rolls can now hand XdY strings

--- lib/dieroll/roll.rb
+++ lib/dieroll/roll.rb
@@ -3,8 +3,17 @@ module Dieroll
     def self.d(sides)
       rand(1..sides)
     end
+
     def self.string(string)
-      
+      num, sides = string.match(/^(\d+)d(\d+)/).captures
+      num = num.to_i
+      sides = sides.to_i
+
+      total = 0
+      num.times do
+        total += Roll.d(sides)
+      end
+      return total
     end
   end
 end
