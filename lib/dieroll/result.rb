efaa4df029662f4334254eca930fc4c7b9df8d13
Scott Reis <scott.reis@vertive.com>
2012-03-12 14:52:29 -0500

    minor readability adjustments

--- lib/dieroll/result.rb
+++ lib/dieroll/result.rb
@@ -2,13 +2,14 @@ module Dieroll
   class Result
     attr_reader :total, :dice, :sides, :string
     
+    # Create result object
     def initialize(sides, dice)
-      @sides = sides
-      @dice = dice
-      @total = dice.inject(0){|sum, x| sum+x}
+      @sides, @dice = sides, dice
+      @total = dice.inject(0) { |sum, x| sum + x }
       @string = "#{@dice.count}d#{@sides}"
     end
     
+    # Return result as string
     def to_s
       "#{@total}:#{@string}:#{@dice.join(",")}"
     end
